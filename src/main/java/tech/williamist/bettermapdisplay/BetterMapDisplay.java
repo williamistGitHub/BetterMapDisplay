5ad88ca646c889ea0641f8f16fd0d95d75ce3a21
williamistGitHub <william@distal.com>
2024-11-17 16:28:40 -0500

    rebrand/reversion

--- src/main/java/tech/williamist/bettermapdisplay/BetterMapDisplay.java
+++ src/main/java/tech/williamist/bettermapdisplay/BetterMapDisplay.java
@@ -1,19 +1,19 @@
-package com.example.example_mod;
+package tech.williamist.bettermapdisplay;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import net.ornithemc.osl.entrypoints.api.ModInitializer;
 
-public class ExampleMod implements ModInitializer {
+public class BetterMapDisplay implements ModInitializer {
 
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("Example Mod");
+	public static final Logger LOGGER = LogManager.getLogger("Better Map Display");
 
 	@Override
 	public void init() {
-		LOGGER.info("initializing example mod!");
+		LOGGER.info("Better Map Display initialized!");
 	}
 }
